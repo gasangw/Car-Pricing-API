6f5b9d1620d32aed11de0a935881b152dd5c40d1
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-12-04 20:00:51 +0200

    refactor: create the hashed password and modifed the user controller to call the authservice instead of the userService

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
 
+const scrypt = promisify(_scrypt);
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
+  async signup(email: string, password: string) {
+    // see if email is in use
+    const users = await this.usersService.find(email);
+    if (users.length) {
+      throw new BadRequestException('email in use');
+    }
+    // create salt
+    // hash the users password
+    const salt = randomBytes(8).toString('hex');
+    const hashedPassword = (await scrypt(password, salt, 32)) as Buffer;
+    const result = `${salt}.${hashedPassword.toString('hex')}`;
+    // create a new users and save it
+    const user = this.usersService.create(email, result);
+    // return the users
+    return user;
+  }
+
+  signin() {}
 }

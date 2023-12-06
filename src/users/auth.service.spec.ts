9685825c16184b31bc9a237304ec4e065a87a32d
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-12-06 16:25:34 +0200

    refactor: add test to throw an error if the email signed in with is already in use

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,29 +2,49 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
+import { UsersController } from './users.controller';
 
 describe('The authservice', () => {
   let service: AuthService;
+  let fakeUserService: Partial<UsersService>;
 
   beforeEach(async () => {
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUserService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password } as User),
     };
     const module = await Test.createTestingModule({
+      controllers: [UsersController],
       providers: [
         AuthService,
         {
           provide: UsersService,
-          useValue: fakeUsersService,
+          useValue: fakeUserService,
         },
       ],
     }).compile();
     service = module.get(AuthService);
   });
-});
 
-test('should return an instance of the authservice', async () => {
-  expect(service).toBeDefined();
+  test('should return an instance of the authservice', async () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should create a new user with a salted and hashed password', async () => {
+    const user = await service.signup('thomas@gmail.com', 'thom123');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+
+  it('should throw an error if the email is in use', async () => {
+    fakeUserService.find = () =>
+      Promise.resolve([{ id: 1, email: 'thom', password: '123' }]);
+    try {
+      await service.signin('thomas@gmail.com', 'tomi333');
+    } catch (error) {
+      done();
+    }
+  });
 });
